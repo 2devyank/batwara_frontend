edf40539ae50a7c9cb0dc9b0de1048ae4637334b
devyank <devyanknagpal2002@gmail.com>
2023-03-02 23:13:53 +0530

    add member functionality done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,14 @@ import s1 from "../assets/s1.png"
 import h1 from "../assets/h1.png"
 import jg from "../assets/jg.png"
 import user from "../assets/user.png"
-import { Link, useNavigate } from 'react-router-dom'
-import { Table } from 'react-bootstrap'
+import {  Link, useNavigate } from 'react-router-dom'
+import { Form,Button, Modal, Table } from 'react-bootstrap'
 import { useUserAuth } from '../context/UserAuthcontext'
 function Dashboard() {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  // const handleClose = () => setShow(false);
+ 
   const [grpdata, setgrpdata] = useState([])
   const [persondata, setpersondata] = useState([])
   const [loading, setloading] = useState(false);
@@ -53,6 +57,58 @@ function Dashboard() {
   // console.log(username);
   // console.log(grpdata);
   // console.log(persondata[0].email);
+
+
+
+
+
+  const [grpname, setgrpname] = useState("");
+  const [input, setinput] = useState("");
+  const [grpmember, setgrpmember] = useState([]);
+  const [list, setlist] = useState([]);
+
+
+  useEffect(()=>{
+getmember(input);
+  },[input]);
+  async function getmember(query){
+    // e.preventDefault();
+    const result=await fetch(`http://localhost:5000/user/${query}`)
+    const data=await result.json();
+setlist(data);
+
+  }
+  console.log(list[0]);
+
+
+
+const handleadd=(e)=>{
+  e.preventDefault();
+  setgrpmember(prevState => [...prevState,list[0].name]);
+  // setlist('')
+  setinput('')
+
+
+}
+console.log(grpmember);
+
+async function postgroup(e){
+  let person_id=localStorage.getItem("pid")
+  let group_id=Math.random().toString(6).slice(-5);
+  try{
+
+    const body={group_id,person_id,grpname,grpmember}
+    const result=await fetch("http://localhost:5000/group",{
+      method:"POST",
+      headers:{"Content-Type":"Application/json"},
+      body:JSON.stringify(body)
+    })
+    setShow(false);
+  }catch(e){
+    console.log(e)
+  }
+}
+
   return (
     <>
 
@@ -65,7 +121,7 @@ function Dashboard() {
             </Link>
             </span>
 
-            <button className='addgroup'> <img src={jg} alt="" style={{ width: "30px", height: "30px" }} />Add Group +</button>
+            <button className='addgroup' onClick={handleShow}> <img src={jg} alt="" style={{ width: "30px", height: "30px" }} />Add Group +</button>
             <button className='addgroup'> <img src={user} alt="" style={{ width: "30px", height: "30px" }} />Add Member +</button>
           </div>
           <div className='belowmenu'>
@@ -208,6 +264,51 @@ function Dashboard() {
           </Table>
         </div>
       </div>
+
+
+
+      <Modal show={show} onHide={postgroup}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+<Form>
+  
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Group Name</Form.Label>
+    <Form.Control type="group name" placeholder="Enter Group Name" value={grpname} onChange={(e)=>setgrpname(e.target.value)}/>
+ 
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Add Group Member</Form.Label>
+    {/* {grpmember.map((data)=>{
+return <span>{data}</span>
+    }
+  )} */}
+    <Form.Control type="name" placeholder="Enterrr Member" value={input} onChange={(e)=>setinput(e.target.value)}/>
+  
+  {list.map((li)=>{
+ return<div>
+
+ <span>{li.email}</span>
+ <button onClick={handleadd}>ADD</button>
+ </div> 
+})}
+  </Form.Group>
+  
+</Form>
+
+
+
+      </Modal.Body>
+      <Modal.Footer>
+        
+        <Button variant="primary" onClick={postgroup}>
+          CREATE GROUP
+        </Button>
+      </Modal.Footer>
+    </Modal>
 
     </>
   )
